fc3dd69c452471107031d1553495c98a8132db18
Santiago <aviles.barrientos.santiago.sa@gmail.com>
2024-11-20 22:51:48 -0400

    refactoring: simplificacion de funcion de calculo y mejora de manejo de los delimitadores

--- src/calcString.js
+++ src/calcString.js
@@ -1,24 +1,38 @@
+// Función para procesar delimitadores personalizados
+function obtenerDelimitadores(string) {
+    const delimitadorPorDefecto = /[,|-]/;
 
-function calcular (string) {
-    if(string ==" "){
-        return "0"
-    }
-    let delimitadores = /[,|-]/
-
-    if(string.startsWith("//[")){
+    if (string.startsWith("//[")) {
         const match = string.match(/\/\/(\[.+\])/);
-        if(match){
-            const delimitadorPersonalizado = match[1].slice(1,-1).split("][")
-            const delimitadoresRegex = delimitadorPersonalizado.map(delim => 
-                delim.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&")
-            ).join("|");
-            delimitadores = new RegExp(`${delimitadoresRegex}|,|-`); 
-            string = string.replace(match[0], "");
+        if (match) {
+            const delimitadores = match[1].slice(1, -1).split("][");
+            const delimitadoresRegex = delimitadores
+                .map(delim => delim.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&"))
+                .join("|");
+            return new RegExp(`${delimitadoresRegex}|,|-`);
         }
     }
-    const numeros = string.split(delimitadores).map(Number).filter(num => num <= 1000)
-    const result = numeros.reduce((sum, num) => sum + num, 0);
-    return result.toString();
+
+    return delimitadorPorDefecto; 
+}
+
+function calcular(string) {
+    const input = string.trim();
+    if (!input) {
+        return "0";
+    }
+
+    const delimitadores = obtenerDelimitadores(string);
+
+    if (string.startsWith("//[")) {
+        string = string.replace(/\/\/(\[.+\])/, "");
+    }
+
+    const numeros = string.split(delimitadores)
+        .map(Number)
+        .filter(num => num <= 1000);
+
+    return numeros.reduce((sum, num) => sum + num, 0).toString();
 }
 
 export default calcular;
